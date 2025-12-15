15cbe8acb71044eef2c69fd02b6b2275401f64ee
Brainiac <74297323+BabalolaBrainiac@users.noreply.github.com>
2025-12-15 03:35:40 +0000

    add firewall and fix ssh variable

--- hetzner/ssh.tf
+++ hetzner/ssh.tf
@@ -1,15 +1,15 @@
 data "hcloud_ssh_key" "existing" {
-  count = var.ssh_key_name != "" && var.ssh_public_key == "" ? 1 : 0
+  count = var.ssh_key_name != "" && length(trimspace(var.ssh_public_key)) == 0 ? 1 : 0
   name  = var.ssh_key_name
 }
 
 resource "hcloud_ssh_key" "main" {
-  count = var.ssh_public_key != "" ? 1 : 0
+  count = length(trimspace(var.ssh_public_key)) > 0 ? 1 : 0
 
   name       = var.ssh_key_name != "" ? var.ssh_key_name : "${var.project_name}-${var.environment}-ssh-key"
   public_key = var.ssh_public_key
 }
 
 locals {
-  ssh_key_id = var.ssh_public_key != "" ? hcloud_ssh_key.main[0].id : data.hcloud_ssh_key.existing[0].id
+  ssh_key_id = length(trimspace(var.ssh_public_key)) > 0 ? hcloud_ssh_key.main[0].id : (var.ssh_key_name != "" ? data.hcloud_ssh_key.existing[0].id : null)
 }
